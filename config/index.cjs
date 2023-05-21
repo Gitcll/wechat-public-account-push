3061423b5ba296a46c2cb7bac18b75dd171eb182
chengll <46524944+Gitcll@users.noreply.github.com>
2023-05-21 21:57:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,45 +1,138 @@
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
 const USER_CONFIG = {
-	"APP_ID": "",
-	"APP_SECRET": "",
-	"IS_SHOW_COLOR": true,
-	"USERS": [
-		{
-			"name": "宝贝",
-			"id": "oCIxo63CHmMlqDFNLLuX-gOMrsVM",
-			"useTemplateId": "dO3dbdhlC-WScTpga78E37fHhE7gzV43wZ9xgDLuCtk",
-			"province": "陕西省",
-			"city": "西安市",
-			"horoscopeDate": "05-21",
-			"horoscopeDateType": "今日",
-			"openUrl": "https://shuangxunian.github.io/",
-			"festivals": [
-				{
-					"type": "*生日",
-					"name": "认识纪念日",
-					"date": "04-06",
-					"year": "2023"
-				}
-			],
-			"customizedDateList": []
-		}
-	],
-	"CALLBACK_TEMPLATE_ID": "_GoAwE7HxMXVpMzQsfIibjSOvy-gw81UvANFzJe8c3E",
-	"CALLBACK_USERS": [
-		{
-			"name": "自己",
-			"id": "oCIxo63CHmMlqDFNLLuX-gOMrsVM"
-		}
-	],
-	"SWITCH": {
-		"weather": true,
-		"holidaytts": true,
-		"CIBA": true,
-		"oneTalk": true,
-		"earthyLoveWords": true,
-		"momentCopyrighting": true,
-		"poisonChickenSoup": true,
-		"poetry": true,
-		"horoscope": true,
-		"birthdayMessage": true
-	}
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: '',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: '',
+
+  PROVINCE: '陕西省',
+  CITY: '西安市',
+  LITERARY_PREFERENCE: '',
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oCIxo63CHmMlqDFNLLuX-gOMrsVM',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'vmkfCkEMJ33WDcZL30qRFPTbjAN_27hRbBJ1duHuI3s',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '节日', name: '认识纪念日', year: '2023', date: '03-18',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-03-18' }
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '_GoAwE7HxMXVpMzQsfIibjSOvy-gw81UvANFzJe8c3E',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oCIxo63CHmMlqDFNLLuX-gOMrsVM',
+    }
+  ],
+
+    SLOT_LIST: [
+    // 这样配置的话，就会每次发送这句话
+    { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
+    // 这样配置的话，就会每次随机选一句话发送
+    {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为太喜欢你，所以看谁都像是情敌。',
+        '申请成为你爱里的永久居民。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '遇见你，就好像捡到了100斤的运气'
+      ],
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+  
+  
+  // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: true,
+    
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: true,
+    
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: true,
+    
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: true,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: true,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: true,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: true,
+  },
+  
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: true,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 3,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
 }
+
+module.exports = USER_CONFIG
+
