5eeaf51c8ced9c620dfc76fed84e68924f675f97
chengll <46524944+Gitcll@users.noreply.github.com>
2023-05-21 19:54:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxd1491ac6757a5227',
+      id: 'oCIxo63CHmMlqDFNLLuX-gOMrsVM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'o4CvCNg6Ti_P_N08cU-3LSEYS6SvhqUwd0MQh7jftOs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'wxd1491ac6757a5227',
+  CALLBACK_TEMPLATE_ID: '1ksOKX9pfQVijvn2KjD7IXLG7dQ6vBmLdnFhG62lVhQ',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxd1491ac6757a5227',
+      id: 'oCIxo63CHmMlqDFNLLuX-gOMrsVM',
     }
   ],
 
