1a9ad5f4d1959268e5185d833097017e50fc776d
chengll <46524944+Gitcll@users.noreply.github.com>
2023-05-21 21:53:05 +0800

    Update index_bak.cjs

--- config/index_bak.cjs
+++ config/index_bak.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCIxo63CHmMlqDFNLLuX-gOMrsVM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dO3dbdhlC-WScTpga78E37fHhE7gzV43wZ9xgDLuCtk',
+      useTemplateId: 'vmkfCkEMJ33WDcZL30qRFPTbjAN_27hRbBJ1duHuI3s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
